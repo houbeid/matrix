cd21830a5ee38d346ec447b077da3fdaa5cc890f
Oubeid Hamad <OubeidH@algoconsulting.net>
2025-08-10 11:58:06 +0100

    modified lineare interpolation

--- ex02/lineare_interpolation.py
+++ ex02/lineare_interpolation.py
@@ -9,6 +9,15 @@ class Vector:
     def __init__(self, values: List[Number]):
         self.values = values
 
+    def __len__(self):
+        return len(self.values)
+
+    def __getitem__(self, index: int):
+        return self.values[index]
+
+    def __str__(self):
+        return '\n'.join([f"[{x}]" for x in self.values])
+
     def lerp(self, other: 'Vector', t: float) -> 'Vector':
         """Interpolation entre deux vecteurs."""
         if len(self.values) != len(other.values):
@@ -37,8 +46,8 @@ class Matrix:
             raise ValueError("Les matrices doivent avoir le même nombre de lignes.")
         return Matrix([self.rows[i].lerp(other.rows[i], t) for i in range(len(self.rows))])
 
-    def __repr__(self) -> str:
-        return "\n".join(str(row) for row in self.rows)
+    def __repr__(self):
+        return "[" + ",\n ".join(str(row.values) for row in self.rows) + "]"
 
 
 def main():
@@ -54,55 +63,13 @@ def main():
     # ---- VECTEUR ----
     u_vec = Vector([2., 1.])
     v_vec = Vector([4., 2.])
-    print(u_vec.lerp(v_vec, 0.3))
+    print("// ", u_vec.lerp(v_vec, 0.3))
 
     # ---- MATRICE ----
     u_mat = Matrix([Vector([2., 1.]), Vector([3., 4.])])
     v_mat = Matrix([Vector([20., 10.]), Vector([30., 40.])])
-    result = u_mat.lerp(v_mat, 0.5)
-    print(result)
+    print("// ", u_mat.lerp(v_mat, 0.5))
 
 
 if __name__ == "__main__":
     main()
-
-
-# import math
-# from typing import Union, List
-
-# Number = Union[int, float]
-
-# def lerp_value(u: Number, v: Number, t: float) -> float:
-#     return math.fma(t, v - u, u)
-
-# def lerp_vector(u: List[Number], v: List[Number], t: float) -> List[float]:
-#     if len(u) != len(v):
-#         raise ValueError("Les vecteurs doivent avoir la même taille.")
-#     return [math.fma(t, v[i] - u[i], u[i]) for i in range(len(u))]
-
-# def lerp_matrix(u: List[List[Number]], v: List[List[Number]], t: float) -> List[List[float]]:
-#     if len(u) != len(v) or len(u[0]) != len(v[0]):
-#         raise ValueError("Les matrices doivent avoir la même taille.")
-#     return [
-#         [math.fma(t, v[i][j] - u[i][j], u[i][j]) for j in range(len(u[0]))]
-#         for i in range(len(u))
-#     ]
-
-# def main():
-#     print(lerp_value(0., 1., 0.)) 
-#     print(lerp_value(0., 1., 1.)) 
-#     print(lerp_value(0., 1., 0.5))
-#     print(lerp_value(21., 42., 0.3))
-
-#     u_vec = [2., 1.]
-#     v_vec = [4., 2.]
-#     print(lerp_vector(u_vec, v_vec, 0.3))
-
-#     u_mat = [[2., 1.], [3., 4.]]
-#     v_mat = [[20., 10.], [30., 40.]]
-#     result = lerp_matrix(u_mat, v_mat, 0.5)
-#     for row in result:
-#         print(row)
-
-# if __name__ == "__main__":
-#    main()
